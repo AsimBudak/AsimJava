f099dbab9898b30bf8e1cea244ed6c421cf7ef24
AsimBudak <abudak2003@gmail.com>
2022-07-26 23:49:11 +0300

    StaticBlock & PassByValue

--- src/day26_StaticKeyword/C09_PassByValue.java
+++ src/day26_StaticKeyword/C09_PassByValue.java
@@ -5,9 +5,18 @@ public class C09_PassByValue {
     //iki yontem kullanabiliriz
     //1-verianle'i class level'da static olarak olusturabilirim
 
-static int etiketFiyati;
+static double etiketFiyati;
+static double indirimYuzdesi;
 
     public static void main(String[] args) {
 
+        etiketFiyati=100;
+        indirimYuzdesi=10;
+        kaliciIndirimYap();
+    }
+
+    private static void kaliciIndirimYap() {
+        etiketFiyati=etiketFiyati*(100-indirimYuzdesi)/100;
+        System.out.println("indirimli fiyat : "+etiketFiyati);
     }
 }
